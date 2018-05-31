14c66237aba7b5052685aafbeb89f48458e8eca7
Utkarsh Gaur <charmedtobe@gmail.com>
2018-05-31 14:53:49 -0400

    Hello World in 4 languages

---
+++ Hello World/hello.cpp
@@ -0,0 +1,14 @@
+//This is an inline comment
+
+/*
+    This is a multi-line comment
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hellow\n";
+    return 0;
+}
+
---
+++ Hello World/hello.py
@@ -0,0 +1,6 @@
+# This is an inline comment
+
+# Python has no multi-line comment but it does have docstrings
+# which document the code in a very specific way (not shown here)
+
+print("Hi world")
